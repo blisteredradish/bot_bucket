ed220d5956233b4b9d1cf06187b55089805f2293
blisteredradish <nick.muscolo@gmail.com>
2015-10-29 11:58:41 -0700

    add kill command

--- h_soundsboard_project/workingSoundBoard.py
+++ h_soundsboard_project/workingSoundBoard.py
@@ -14,16 +14,15 @@ def populate_sound_list():
 
 populate_sound_list()
 
-
-
-
 class soundWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="sound window")
         self.set_default_size(200,200)
-
+        box0=Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
         box1=Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-
+        box2=Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        
+##SOUND PORTION
         file=open('./sound.txt')
         for i in file:
             y=i
@@ -34,8 +33,27 @@ class soundWindow(Gtk.Window):
         button0=Gtk.Button.new_with_label("Kill Sound")
         button0.connect("clicked", self.kill_sound)
         box1.pack_start(button0,True,True,0)
-        self.add(box1)        
-            
+##LIGHT PORTION
+        
+        lightFile=open('./light.txt')
+        
+        for i in lightFile:
+            y=i
+            i=Gtk.Button.new_with_label(i)
+            i.connect("clicked", self.display_light,y)
+            box2.pack_start(i,True,True,0)
+        
+        lightFile.close()
+        lightButton0=Gtk.Button.new_with_label("kill message")
+        lightButton0.connect("clicked", self.kill_xmessage)
+        box2.pack_start(lightButton0,True,True,0)
+        
+        
+        box0.pack_start(box1,True,True,0)
+        box0.pack_start(box2,True,True,0)
+        self.add(box0)
+        
+##SOUND FUNCTIONS            
     def play_sound(self,widget,y):
 ##      file=open('./sound.txt')
 ##      for i in file:
@@ -44,7 +62,14 @@ class soundWindow(Gtk.Window):
 
     def kill_sound(self,widget):
         os.system('killall mplayer &')
-                  
+##LIGHT FUNCTIONS
+    def display_light(self,widget,y):
+        os.system('xmessage {} &'.format(y.rstrip('\n')))
+
+    def kill_xmessage(self,widget):
+        os.system('killall xmessage')
+
+        
 soundWindow().connect("delete-event", Gtk.main_quit)
 soundWindow().show_all()
 Gtk.main()
