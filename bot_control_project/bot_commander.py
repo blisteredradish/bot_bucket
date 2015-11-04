8d270e76f9dbbaa7ae043c758fe87ed7f6bdd7fc
blisteredradish <nick.muscolo@gmail.com>
2015-11-04 12:53:05 -0800

    polished control

--- bot_control_project/bot_commander.py
+++ bot_control_project/bot_commander.py
@@ -2,9 +2,13 @@
 
 from gi.repository import Gtk, GObject
 import serial
-ser=serial.Serial('/dev/ttyUSB0',9600)
+ser=serial.Serial(None,9600)##replace with /dev/ttyUSB0
 
-
+UP_IMG=Gtk.Image.new_from_file('../../arrow-up.png')
+DOWN_IMG=Gtk.Image.new_from_file('../../arrow-down.png')
+RIGHT_IMG=Gtk.Image.new_from_file('../../arrow-right.png')
+LEFT_IMG=Gtk.Image.new_from_file('../../arrow-left.png')
+DIR_LABEL=Gtk.Label("Use Keypad")
 class controlWindow(Gtk.Window):
     def __init__(self):
 
@@ -12,28 +16,29 @@ class controlWindow(Gtk.Window):
         
         Gtk.Window.__init__(self,title="Bot Commander")
         self.set_position(Gtk.WindowPosition.CENTER)
-        self.set_size_request(240,150)
-            
+        self.set_size_request(240,150) 
         box1=Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-        box2=Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-        box3=Gtk.Box()
-        box3.set_homogeneous(False)
         grid1=Gtk.Grid()
+        title_box=Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        button_box=Gtk.Box()
+        dir_grid=Gtk.Grid()
+        button_box.set_homogeneous(False)
+        button_grid=Gtk.Grid()
         
 ##Window Label Section
         label1=Gtk.Label("Robot Commander")
         label2=Gtk.Label("Now with Arrow Keys!")
-        box2.pack_start(label1,True,True,0)
-##        box2.pack_start(label2,True,True,0)
+        title_box.pack_start(label1,True,True,0)
+##        title_box.pack_start(label2,True,True,0)
 ##Button Section
         button1=Gtk.Button.new_with_label("1")
-        button2=Gtk.Button.new_with_label("2")
+        button2=Gtk.Button.new_with_label("v")
         button3=Gtk.Button.new_with_label("3")
-        button4=Gtk.Button.new_with_label("4")
+        button4=Gtk.Button.new_with_label("<")
         button5=Gtk.Button.new_with_label("5")
-        button6=Gtk.Button.new_with_label("6")
+        button6=Gtk.Button.new_with_label(">")
         button7=Gtk.Button.new_with_label("7")
-        button8=Gtk.Button.new_with_label("8")
+        button8=Gtk.Button.new_with_label("^")
         button9=Gtk.Button.new_with_label("9")
         button0=Gtk.Button.new_with_label("0")
 
@@ -42,31 +47,40 @@ class controlWindow(Gtk.Window):
         self.connect("key-press-event", self.key_watch)
         self.connect("key-release-event", self.key_release)
 
-        grid1.add(button0)
-        grid1.attach_next_to(button2,button0,Gtk.PositionType.TOP,1,1)
-        grid1.attach_next_to(button1,button2,Gtk.PositionType.LEFT,1,1)
-        grid1.attach_next_to(button3,button2,Gtk.PositionType.RIGHT,1,1)
-        grid1.attach_next_to(button4,button1,Gtk.PositionType.TOP,1,1)
-        grid1.attach_next_to(button5,button2,Gtk.PositionType.TOP,1,1)
-        grid1.attach_next_to(button6,button3,Gtk.PositionType.TOP,1,1)
-        grid1.attach_next_to(button7,button4,Gtk.PositionType.TOP,1,1)
+        button_grid.add(button0)
+        button_grid.attach_next_to(button2,button0,Gtk.PositionType.TOP,1,1)
+        button_grid.attach_next_to(button1,button2,Gtk.PositionType.LEFT,1,1)
+        button_grid.attach_next_to(button3,button2,Gtk.PositionType.RIGHT,1,1)
+        button_grid.attach_next_to(button4,button1,Gtk.PositionType.TOP,1,1)
+        button_grid.attach_next_to(button5,button2,Gtk.PositionType.TOP,1,1)
+        button_grid.attach_next_to(button6,button3,Gtk.PositionType.TOP,1,1)
+        button_grid.attach_next_to(button7,button4,Gtk.PositionType.TOP,1,1)
 
-        grid1.attach_next_to(button8,button5,Gtk.PositionType.TOP,1,1)
-        grid1.attach_next_to(button9,button6,Gtk.PositionType.TOP,1,1)
+        button_grid.attach_next_to(button8,button5,Gtk.PositionType.TOP,1,1)
+        button_grid.attach_next_to(button9,button6,Gtk.PositionType.TOP,1,1)
 
-        box3.pack_start(grid1,True,True,0)
-
+        button_box.pack_start(button_grid,True,True,0)
         
-        box1.pack_start(box2,True,True,0)
-        box1.pack_start(box3,True,True,0)
+        dir_grid.attach(UP_IMG,   1,1,1,1)
+        dir_grid.attach(DOWN_IMG, 1,3,1,1)
+        dir_grid.attach(LEFT_IMG, 0,2,1,1)
+        dir_grid.attach(RIGHT_IMG,2,2,1,1)
+        dir_grid.attach_next_to(DIR_LABEL,UP_IMG,Gtk.PositionType.TOP,1,1)
+##        grid1.attach(title_box,1,-1,1,1)
+        grid1.attach(button_box,0,0,1,1)
+        grid1.attach(dir_grid,1,0,1,1)
+        
 
+        box1.pack_start(title_box,True,True,0)
+        box1.pack_start(grid1,True,True,0)
+        
         self.add(box1)
         
     def button8_pressed(self,widget):
-        print("Pressed")
-
+        print("UP Pressed")
+        
     def button_released(self,widget):
-        print("Released")
+        print("UP Released")
 
         
     def key_release(self,widget,event):
@@ -75,8 +89,7 @@ class controlWindow(Gtk.Window):
     def key_watch(self,widget,event):
         if event.keyval==65362:
             print("UP")
-            ser.write(b'1')
-          
+            ser.write(b'1')       
         elif event.keyval==65364:
             print("DOWN")
             ser.write(b'2')
@@ -87,7 +100,7 @@ class controlWindow(Gtk.Window):
             print("RIGHT")
             ser.write(b'4')
                     
-
-controlWindow().connect("delete-event", Gtk.main_quit)
-controlWindow().show_all()
+cwindow=controlWindow()
+cwindow.connect("delete-event", Gtk.main_quit)
+cwindow.show_all()
 Gtk.main()
